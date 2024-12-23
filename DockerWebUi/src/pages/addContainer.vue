d81c3defba47a7210be0c22fb5c4c796c1c0eb30
RikiLaNeko <arthur.mortreux@lyceeastier.com>
2024-12-24 00:40:00 +0100

    Update on mar. 24 déc. 2024 00:40:00 CET at 00:40:00

---
+++ DockerWebUi/src/pages/addContainer.vue
@@ -0,0 +1,14 @@
+<<template>
+  <div>
+    <h1>Add Container</h1>
+    <form @submit.prevent="addContainer">
+      <div class="form-group mb-3">
+        <label for="containerName">Container Name</label>
+        <input type="text" class="form-control" id="containerName" v-model="containerName" required>
+      </div>
+      <div class="form-group mb-3">
+        <label for="containerImage">Container Image</label>
+        <input type="text" class="form-control" id="containerImage" v-model="containerImage" required>
+      </div>
+
+</template>
