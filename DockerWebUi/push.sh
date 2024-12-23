93f668ff042edb795a2c13e98d568db8ec2c58ca
RikiLaNeko <arthur.mortreux@lyceeastier.com>
2024-12-23 17:03:50 +0100

    Update on lun. 23 déc. 2024 17:03:50 CET at 17:03:50

--- DockerWebUi/push.sh
+++ DockerWebUi/push.sh
@@ -4,7 +4,7 @@
 git add .
 
 # Commit with the date and time of the commit 
-git commit -m "Update on $(date)"
+git commit -m "Update on $(date) at $(date +%H:%M:%S)"
 
 # Push to the remote repository
 git push origin master
