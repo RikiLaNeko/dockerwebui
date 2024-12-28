53d2efb162ac5fb57d1643f99602c0f2fbe6265c
RikiLaNeko <arthur.mortreux@lyceeastier.com>
2024-12-28 22:52:16 +0100

    Update on sam. 28 déc. 2024 22:52:16 CET at 22:52:16

---
+++ src/components/AddContainer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Add New Container</h1>
+    <form @submit.prevent="submitForm">
+      <div>
+        <label for="name">Container Name:</label>
+        <input type="text" v-model="name" id="name" required>
+      </div>
+      <div>
+        <label for="image">Image:</label>
+        <input type="text" v-model="image" id="image" required>
+      </div>
+      <button type="submit">Add Container</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'AddContainer',
+  setup() {
+    const name = ref('');
+    const image = ref('');
+    const serverURL = 'http://localhost:3000';
+
+    const submitForm = async () => {
+      try {
+        await axios.post(serverURL + '/api/containers/create', {
+          name: name.value,
+          image: image.value
+        });
+        // Redirect to home page after successful creation
+        this.$router.push('/');
+      } catch (error) {
+        console.error('Error adding container:', error);
+      }
+    };
+
+    return {
+      name,
+      image,
+      submitForm
+    };
+  }
+});
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
